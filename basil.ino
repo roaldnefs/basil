ad971caf370f15fd984cdb215d2558a72440f4b3
Roald Nefs <info@roaldnefs.com>
2021-02-03 18:51:21 +0100

    feat: setup moisture sensor
    Signed-off-by: Roald Nefs <info@roaldnefs.com>

--- basil.ino
+++ basil.ino
@@ -1,14 +1,34 @@
-# define LED D4
+#define ANA A0
+#define DIGI D5
+
+
+double analogValue = 0.0;
+int digitalValue = 0;
+double analogVolts = 0.0;
+unsigned long timeHolder = 0;
 
 
 void setup() {
-    pinMode(LED, OUTPUT);
+    // Setup the analog and digital input pins
+    pinMode(ANA, INPUT);
+    pinMode(DIGI, INPUT);
+
+    // Setup serial connection
+    Serial.begin(115200);
+    Serial.println("Serial setup");
 }
 
 
 void loop() {
-    digitalWrite(LED, HIGH);
-    delay(1000);
-    digitalWrite(LED, LOW);
+    // Read the pin values
+    analogValue = analogRead(ANA);
+    digitalValue = digitalRead(DIGI);
+
+    // Print the latest readings to the serial console
+    Serial.print("Analog raw: ");
+    Serial.println(analogValue);
+    Serial.print("Digital raw: ");
+    Serial.println(digitalValue);
+    Serial.println(" ");
     delay(1000);
 }
